f68f5ed7ed6a7c16fa58f31e5deca2f310c58e99
codeatusman <104136892+codeatusman@users.noreply.github.com>
2024-01-09 15:47:19 +0500

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,136 @@
+export const LINKS = [
+  {
+    title: 'home',
+    path: 'home',
+    offset: -50,
+  },
+  {
+    title: 'listing',
+    path: 'listing',
+    offset: -50,
+  },
+  {
+    title: 'how it works',
+    path: 'video',
+    offset: -50,
+  },
+  {
+    title: 'testimonials',
+    path: 'testimonials',
+    offset: -50,
+  },
+]
+export const CARS = [
+  {
+    title: 'Mitsubishi Pajero',
+    rent: 399.00,
+    url: '/image1.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+  {
+    title: 'Buick LaCrosse',
+    rent: 399.00,
+    url: '/image2.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+  {
+    title: 'Mazda LaPuta',
+    rent: 399.00,
+    url: '/image3.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+  {
+    title: 'Skoda Laura',
+    rent: 399.00,
+    url: '/image7.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+  {
+    title: 'Honda Fitta',
+    rent: 399.00,
+    url: '/image5.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+  {
+    title: 'Nissan Moco',
+    rent: 399.00,
+    url: '/image6.jpg',
+    transmission: 'auto',
+    luggage: 8,
+    passenger: 4,
+  },
+]
+
+// TESTIMONIAL SECTION
+export const TESTIMONIAL = [
+  {
+    title: "James Martinez",
+    profession: "Director, Nova Gold",
+    URL: "/person-1.jpg",
+    desc: "Fantastic destinations, knowledgeable guides, seamless logistics. Spectacular views and memories. Exceeded expectations; worth every penny",
+  },
+  {
+    title: "Sophia Anderson",
+    profession: "Manager, Safe City",
+    URL: "/person-2.jpg",
+    desc: "Unforgettable experiences, friendly staff, and top-notch organization. Highly recommended for adventurous souls. A must-try. Highly recommend",
+  },
+  {
+    title: "Lily Walker",
+    profession: "Director, High Gold",
+    URL: "/person-3.jpg",
+    desc: "Unbelievable sights, professional guides, seamless planning and adventure. Unmatched experiences that leave you wanting more. Highly recommended",
+  },
+  {
+    title: "Ninna Williams",
+    profession: "CEO, Rafa Builds",
+    URL: "/person-4.jpg",
+    desc: "Unparalleled expertise, personalized service, breathtaking destinations. Consistently delivers an extraordinary travel experience. Highly recommended",
+  },
+  {
+    title: "Ali Khan",
+    profession: "CEO, High Chase",
+    URL: "/person-5.jpg",
+    desc: "Outstanding journeys, knowledgeable guides, seamless execution. Each trip exceeds expectations, ensuring lasting memories and remarkable experiences.",
+  },
+];
+
+export const FOOTERLINKS = [
+  {
+    heading: "About",
+    links: [
+      { title: "How it works", url: "/" },
+      { title: "Featured", url: "/" },
+      { title: "Partnership", url: "/" },
+      { title: "Bussiness Relation", url: "/" },
+    ],
+  },
+  {
+    heading: "Company",
+    links: [
+      { title: "Events", url: "/" },
+      { title: "Blog", url: "/" },
+      { title: "Podcast", url: "/" },
+      { title: "Invite a friend", url: "/" },
+    ],
+  },
+  {
+    heading: "Socials",
+    links: [
+      { title: "Discord", url: "/" },
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+    ],
+  },
+];
---
+++ tailwind.config.ts
@@ -0,0 +1,75 @@
+import type { Config } from "tailwindcss"
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: '#111',
+        secondary: '#4d74ff',
+        tertiary: '#202020',
+        slate: {
+          10: '#f1f3f4',
+        },
+        gray: {
+          10: '#EEEEEE',
+          20: '#A2A2A2',
+          30: '#7B7B7B',
+          50: '#585858',
+          90: '#141414',
+        },
+      },
+     backgroundImage: {
+        hero: "url('/hero.jpg')",
+        video: "url('/video2.jpg')",
+        pattern: "url('/pattern.png')",
+        'hero-bg': "url('/hero-bg.png')",
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
